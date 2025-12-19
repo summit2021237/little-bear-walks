7f97f7ba2fccc2b8fc321f55db8425511acf9575
Summit Fan <fank1@rose-hulman.edu>
2025-12-18 18:32:32 -0700

    more effective constraints for reducing three-walk days

--- src/modeling_problem.py
+++ src/modeling_problem.py
@@ -12,18 +12,19 @@ M.Dates = Set()
 M.Times = Set()
 
 # Parameters
-M.WalkNeeded = Param(M.Times, M.Dates, within=Binary)
-M.Ratings = Param(M.Times, M.Dates, M.People, within=NonNegativeIntegers)
+M.WalkNeeded = Param(M.Times, M.Dates, default=1, within=Binary)
+M.Ratings = Param(M.Times, M.Dates, M.People, default=5, within=NonNegativeIntegers)
 M.Lengths = Param(M.Times, within=NonNegativeIntegers)
 M.MaxWalkFrac = Param(within=NonNegativeReals)
 M.ThreeWalkFactor = Param(within=NonNegativeReals)
 
 # Variables
 M.Assignments = Var(M.Times, M.Dates, M.People, within=Binary)
+M.IsThreeWalks = Var(M.Dates, M.People, within=Binary)
 
 # Objective
 def CalcSchedule(M):
-    return sum(sum(sum(M.Ratings[t, d, p]*M.WalkNeeded[t, d]*M.Assignments[t, d, p] for p in M.People) for d in M.Dates) for t in M.Times) + M.ThreeWalkFactor*sum(sum(2 - sum(M.Assignments[t, d, p] for t in M.Times) for d in M.Dates) for p in M.People)
+    return sum(sum(sum(M.Ratings[t, d, p]*M.WalkNeeded[t, d]*M.Assignments[t, d, p] for p in M.People) for d in M.Dates) for t in M.Times) - M.ThreeWalkFactor*sum(sum(M.IsThreeWalks[d, p] for d in M.Dates) for p in M.People)
 M.Schedule = Objective(rule=CalcSchedule, sense=maximize)
 
 # Constraints
@@ -38,6 +39,14 @@ M.DurationsSimilar = Constraint(M.People, rule=EnsureDurationsSimilar)
 def EnsurePersonUnavailable(M, t, d, p):
     return M.Assignments[t, d, p] <= M.Ratings[t, d, p]
 M.PersonUnavailable = Constraint(M.Times, M.Dates, M.People, rule=EnsurePersonUnavailable)
+
+def EnsureIsThreeWalksVarLower(M, d, p):
+    return sum(M.Assignments[t, d, p] for t in M.Times) - 2 <= M.IsThreeWalks[d, p]
+M.IsThreeWalksVarLower = Constraint(M.Dates, M.People, rule=EnsureIsThreeWalksVarLower)
+
+def EnsureIsThreeWalksVarUpper(M, d, p):
+    return sum(M.Assignments[t, d, p] for t in M.Times) >= 3*M.IsThreeWalks[d, p]
+M.IsThreeWalksVarUpper = Constraint(M.Dates, M.People, rule=EnsureIsThreeWalksVarUpper)
 
 # Create a problem instance
 instance = M.create_instance("modeling_problem.dat")
