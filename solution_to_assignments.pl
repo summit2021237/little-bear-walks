d99a3182183d496afc7685920f3bfd2881799207
Summit Fan <fank1@rose-hulman.edu>
2025-12-19 13:19:44 -0700

    extracts walk info for each assignment

--- solution_to_assignments.pl
+++ solution_to_assignments.pl
@@ -7,21 +7,43 @@ open(my $out, ">",  "./output/assignments.txt") or die "Can't open assignments.t
 
 # Find start of assignments
 while (<$in>) { # assigns each line to $_
-	$_ =~ s/^\s+|\s+$//g; # remove leading and trailing whitespace from line
+	$_ = trim_whitespace($_);
 	if (/^Assignments.*$/) {
 		<$in>;
 		last;
 	}
 }
 
-# Print assignments
+# Add assigned walks to list
+my @assigned_walks;
 while (<$in>) {
-	$_ =~ s/^\s+|\s+$//g;
+	$_ = trim_whitespace($_);
 	if (!/^\(.*/) {
 		last;
 	}
-	print $out "$_\n";
+
+	my @columns = split /:/;
+	my $is_assigned = $columns[2];
+	$is_assigned = trim_whitespace($is_assigned);
+	if ($is_assigned eq "1.0") {
+		my @walk_info = split /,/, $columns[0];
+		@walk_info = map {extract_single_quote_val($_)} @walk_info;
+		push @assigned_walks, @walk_info;
+		print $out "$walk_info[0], $walk_info[1], $walk_info[2]\n";
+	}
 }
 
 close $in or die "$in: $!";
 close $out or die "$out: $!";
+
+sub trim_whitespace {
+	my $val = $_[0];
+	$val =~ s/^\s+|\s+$//g;
+	return $val;
+}
+
+sub extract_single_quote_val {
+	my $val = $_[0];
+	$val =~ s/^[^']*\'|\'[^']*$//g;
+	return $val;
+}
