45b45061046465eee403715454921f0327f4b094
Summit Fan <fank1@rose-hulman.edu>
2026-01-24 13:51:44 -0700

    writes times with WalkConfig

--- src/WalkConfig.pm
+++ src/WalkConfig.pm
@@ -6,7 +6,16 @@ use warnings;
 use JSON;
 
 sub new {
-	return decode_json($_[1]);
+	my ($class_name, $json) = @_;
+	return bless decode_json($json), $class_name;
+}
+
+sub get_times {
+	my @times = ();
+	foreach my $walk (@{$_[0]->{walk_info}->{walks}}) {
+		push(@times, $walk->{time});
+	}
+	return \@times;
 }
 
 1;
--- src/config_to_dat.pl
+++ src/config_to_dat.pl
@@ -11,5 +11,26 @@ open(my $config_file, "<", "./config.json") or die "Can't open config.json $!";
 my $config_content = do {local $/; <$config_file>};
 close $config_file or die "$config_file: $!";
 
+open(my $dat_file, ">", "./output/modeling_problem_test1.dat") or die "Can't open modeling_problem.dat $!";
+
 my $config = WalkConfig->new($config_content);
-print Dumper($config);
+
+write_sets();
+
+close $dat_file or die "$dat_file $!";
+
+sub write_sets {
+	write_times();
+}
+
+sub write_times{
+	write_to_dat("set Times :=");
+	foreach my $time (@{$config->get_times()}) {
+		write_to_dat(" $time");
+	}
+	write_to_dat(";\n");
+}
+
+sub write_to_dat {
+	print {$dat_file} $_[0];
+}
